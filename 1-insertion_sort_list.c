e9e26a6c155243d244f8a91462d22b9270ae0365
LJThao <yengfong.thao@atlasschool.com>
2024-04-08 20:40:21 +0000

    insertion sort task

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - function that sorts a doubly linked list of ints
+ * @list: list
+ * Return: 0
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *data;
+	listint_t *tp = NULL;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	{
+		return;
+	}
+
+	node = *list;
+
+	while (data != NULL)
+	{
+		tp = data;
+
+		while (tp->prev != NULL && tp->n < tp->prev->n)
+		{
+			if (tp->next != NULL)
+			tp->next->prev = tp->prev;
+
+			tp->prev->next = tp->next;
+			tp->next = tp->prev;
+			tp->prev = tp->prev->prev;
+			tp->next->prev = tp;
+
+			if (tp->prev != NULL)
+			{
+			tp->prev->next = tp;
+			}
+			else
+			{
+				*list = tp;
+			}
+			print_list(*list);
+		}
+			data = tp->next;
+	}
+}
